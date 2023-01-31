38db408392d1a8b04949e548eeadcaccbce44c02
Steve Nguyen <steve.nguyen@pollen-robotics.com>
2023-01-31 17:03:44 +0100

    add ik and fk for head

--- reachy_sdk/head.py
+++ reachy_sdk/head.py
@@ -12,10 +12,21 @@ from scipy.spatial.transform import Rotation
 
 from pyquaternion import Quaternion
 
+from reachy_sdk_api.head_kinematics_pb2_grpc import HeadKinematicsStub
+from reachy_sdk_api.head_kinematics_pb2 import HeadFKRequest, HeadIKRequest
+from reachy_sdk_api.kinematics_pb2 import joint__pb2
+from reachy_sdk_api.arm_kinematics_pb2 import kinematics__pb2
+
 from .device_holder import DeviceHolder
 from .joint import Joint
 from .trajectory import goto, goto_async
 from .trajectory.interpolation import InterpolationMode
+
+
+JointId = joint__pb2.JointId
+JointPosition = kinematics__pb2.JointPosition
+Matrix4x4 = kinematics__pb2.Matrix4x4
+QuaternionPb = kinematics__pb2.Quaternion
 
 
 class Head:
@@ -33,6 +44,8 @@ class Head:
     def __init__(self, joints: List[Joint], grpc_channel) -> None:
         """Set up the head."""
 
+        self._kin_stub = HeadKinematicsStub(grpc_channel)
+
         def get_joint(name):
             for j in joints:
                 if j.name == name:
@@ -42,7 +55,8 @@ class Head:
 
         if len(found_joints) != len(self._required_joints):
             raise ValueError(f'Required joints not found {self._required_joints}')
-
+        self._kinematics_chain = ['neck_roll', 'neck_pitch', 'neck_yaw']
+        self.kinematics_chain = DeviceHolder([j for j in found_joints if j.name in self._kinematics_chain])
         self.joints = DeviceHolder(found_joints)
         self._setup_joints(found_joints)
 
@@ -59,6 +73,79 @@ class Head:
         for j in joints:
             if j.name in self._required_joints:
                 setattr(self, j.name, j)
+
+    def forward_kinematics(self, joints_position: Optional[List[float]] = None) -> Tuple[float, float, float, float]:
+        """Compute the forward kinematics of the head.
+
+        It will return the quaternion (x, y, z, w).
+        You can either specify a given joints position, otherwise it will use the current robot position.
+        """
+        if joints_position is None:
+            joints_position = [j.present_position for j in self.kinematics_chain.values()]
+
+        if isinstance(joints_position, np.ndarray) and len(joints_position.shape) > 1:
+            raise ValueError('Vectorized kinematics not supported!')
+
+        pos = np.deg2rad(list(joints_position))
+
+        if len(pos) != len(self._kinematics_chain):
+            raise ValueError(
+                f'joints_position should be length {len(self._kinematics_chain)} (got {len(pos)} instead)!'
+            )
+
+        req = HeadFKRequest(
+            neck_position=self._joint_position_from_pos(pos),
+
+        )
+        resp = self._kin_stub.ComputeHeadFK(req)
+        if not resp.success:
+            raise ValueError(f'No solution found for the given joints ({joints_position})!')
+
+        return (resp.q.x, resp.q.y, resp.q.z, resp.q.w)
+
+    def inverse_kinematics(self, target: Tuple[float, float, float, float], q0: Optional[List[float]] = None) -> List[float]:
+        """Compute the inverse kinematics of the arm.
+
+        Given a goal quaternion (x, y, z, w)
+        it will try to compute a joint solution to reach this target (or get close).
+
+        It will raise a ValueError if no solution is found.
+
+        You can also specify a basic joint configuration as a prior for the solution.
+        """
+
+        if q0 is not None and (len(q0) != len(self._kinematics_chain)):
+            raise ValueError(f'q0 should be length {len(self._kinematics_chain)} (got {len(q0)} instead)!')
+
+        if isinstance(q0, np.ndarray) and len(q0.shape) > 1:
+            raise ValueError('Vectorized kinematics not supported!')
+
+        qr = QuaternionPb()
+        qr.x = target[0]
+        qr.y = target[1]
+        qr.z = target[2]
+        qr.w = target[3]
+
+        req_params = {
+            'q': qr
+        }
+
+        if q0 is not None:
+            req_params['q0'] = self._joint_position_from_pos(np.deg2rad(q0))
+
+        req = HeadIKRequest(**req_params)
+        resp = self._kin_stub.ComputeHeadIK(req)
+
+        if not resp.success:
+            raise ValueError(f'No solution found for the given target ({target})!')
+
+        return np.rad2deg(resp.neck_position.positions.positions).tolist()
+
+    def _joint_position_from_pos(self, joints_position: List[float]) -> JointPosition:
+        return JointPosition(
+            ids=[JointId(uid=j.uid) for j in self.kinematics_chain.values()],
+            positions=joints_position,
+        )
 
     async def look_at_async(
         self,
